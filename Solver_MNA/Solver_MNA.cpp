d635230e2f7c6e85236c0e16d3dd0f7289ae7dcf
majidahadi <majid.ahadi09@gmail.com>
2014-04-02 17:01:59 -0600

    stamp

--- Solver_MNA/Solver_MNA.cpp
+++ Solver_MNA/Solver_MNA.cpp
@@ -42,25 +42,61 @@ int main()
 		/*tr1::cmatch res;
 		tr1::regex rx("[rlgc]*/
 		vertex[i].type = line[0];
-		cout << vertex[i].type << endl;
-		while(line[j]!=0)
+		flag=0;
+		vertex[i].Na=0;
+		vertex[i].Nb=0;
+		vertex[i].value=0;
+		while(j<line.length())
 		{
-			if (line[j]==' '){
-				flag=1;
-				j++;}
-			if (flag){
-				node[i].
+			if (line[j]==' ')
+				flag++;
+			else{
+			if (flag==1)
+				vertex[i].Na=vertex[i].Na*10+line[j]-48;
+			else if (flag==2)
+				vertex[i].Nb=vertex[i].Nb*10+line[j]-48;
+			else if (flag==3)
+				vertex[i].value=vertex[i].value*10+line[j]-48;
+			}
 			j++;
 		}
+		j=0;
 
 		 	
 		
 		i++;
 		}
-//	for (int j=1; j++; j<i)
-//		cout << vertex[j].type;
+
+	int length=i;
+	for (int p=1; p<length; p++)
+		cout << vertex[p].type << ' ' << vertex[p].Na <<' ' << vertex[p].Nb <<' ' << vertex[p].value << endl;
 	
-	
+	int size=0;
+	for (int p=1; p<length; p++){
+		size=max(size,vertex[p].Na);
+		size=max(size,vertex[p].Nb);
+	}
+	size++;
+	cout << size <<endl;
+	int** A = new int*[size];
+	for(int p = 0; p < size; p++)
+		A[p] = new int[size];
+	for (int p=1; p<size;p++)
+		for (int q=1; q<size; q++)
+			A[p][q]=0;
+
+	for (int p=1;p<length;p++)
+		if (vertex[p].type='g')
+			if (vertex[p].Nb=0)
+				A[vertex[p].Na][vertex[p].Na]=A[vertex[p].Na][vertex[p].Na]+vertex[p].value;
+			else if (vertex[p].Na=0)
+				A[vertex[p].Nb][vertex[p].Nb]=A[vertex[p].Nb][vertex[p].Nb]+vertex[p].value;
+
+	for (int p=1; p<size;p++){
+		for (int q=1; q<size; q++)
+			cout << A[p][q] << ' ';
+		cout << endl;
+	}
 
 	getch();
 	return 0;
