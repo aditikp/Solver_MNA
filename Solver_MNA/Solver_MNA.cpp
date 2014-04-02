637983a67f8009d15f1bd7e60e6dfed42c10979d
aditikp <aditi.krishnaprasad@gmail.com>
2014-04-02 15:32:08 -0600

    updated

--- Solver_MNA/Solver_MNA.cpp
+++ Solver_MNA/Solver_MNA.cpp
@@ -6,12 +6,25 @@
 #include <fstream>
 #include <string>
 #include "conio.h"
+//#include <regex>
 
 using namespace std;
+
+struct node {
+	char type;
+	int Na, Nb;
+	int value;
+};
 
 
 int main()
 {
+	int i=1;
+	int j=1;
+	int flag=0;
+	//	int val;
+//	node *vertex = new node();
+	node vertex[50];
 	ifstream in("circuit.txt");
 	string line;
 
@@ -19,11 +32,36 @@ int main()
 		cerr<<"circuit.txt could not be opened for reading\n"<<endl;
 		exit(1);
 	}
-
+	
 	while(getline(in, line)) {
-		
 		cout << line << endl;
-	}
+//		tr1::regex cond("g");
+//		val = regex_search(line.begin(), line.end(), cond);
+//		cout << "Return Value = " << val << endl;
+
+		/*tr1::cmatch res;
+		tr1::regex rx("[rlgc]*/
+		vertex[i].type = line[0];
+		cout << vertex[i].type << endl;
+		while(line[j]!=0)
+		{
+			if (line[j]==' '){
+				flag=1;
+				j++;}
+			if (flag){
+				node[i].
+			j++;
+		}
+
+		 	
+		
+		i++;
+		}
+//	for (int j=1; j++; j<i)
+//		cout << vertex[j].type;
+	
+	
+
 	getch();
 	return 0;
 }
